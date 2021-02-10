53a392cc587ab0bfcbaff300519534a0ae6a46f0
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-02-10 22:38:41 +0100

    Setup vuetify for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,45 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+const vuetify = new Vuetify()
+
+export const decorators = [
+  (story, context) => {
+    // wrap the passed component within the passed context
+    const wrapped = story(context)
+    // extend Vue to use Vuetify around the wrapped component
+    return Vue.extend({
+      vuetify,
+      components: { wrapped },
+      props: {
+        dark: {
+          type: Boolean,
+          default: context.args.dark,
+        },
+      },
+      watch: {
+        dark: {
+          immediate: true,
+          handler (val) {
+            this.$vuetify.theme.dark = val
+          }
+        }
+      },
+      template: `
+        <v-app>
+          <v-main>
+            <v-container fluid>
+              <wrapped />
+            </v-container>
+          </v-main>
+        </v-app>
+      `
+    })
+  },
+]
--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <v-btn elevation="2" :class="classes" @click="onClick" :style="style">{{ label }}</v-btn>
 </template>
 
 <script>
@@ -32,10 +32,10 @@ export default {
   computed: {
     classes() {
       return {
-        'storybook-button': true,
+        /*'storybook-button': true,
         'storybook-button--primary': this.primary,
         'storybook-button--secondary': !this.primary,
-        [`storybook-button--${this.size}`]: true,
+        [`storybook-button--${this.size}`]: true,*/
       };
     },
     style() {
