4a8c3fed0e0167745fb185e815701be7f845a2fa
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-03-08 21:42:44 +0100

    Move card into slots in page.

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,15 +1,38 @@
 import MyPage from "./Page";
+import MyCard from "./Card.vue";
+import MyInput from "./Input.vue";
+import MyButton from "./Button.vue";
 import * as CardStories from "./Card.stories";
 
 export default {
   title: "Example/Page",
-  component: MyPage
+  component: MyPage,
+  MyCard,
+  MyInput,
+  MyButton
 };
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  components: { MyPage },
-  template: '<my-page @onLogin="onLogin"/>'
+  components: { MyPage, MyCard, MyInput, MyButton },
+  template: `
+    <my-page @onLogin="onLogin">
+      <MyCard
+        title="Login"
+        subTitle="Welcome back. Please login to our service"
+        width="500"
+        elevation="2"
+      >
+        <template v-slot:body>
+          <MyInput label="Login" />
+          <MyInput label="Password" />
+        </template>
+        <template v-slot:footer>
+          <MyButton label="Login" />
+        </template>
+      </MyCard>
+    </my-page>
+  `
 });
 
 export const LoggedOut = Template.bind({});
--- src/stories/Page.vue
+++ src/stories/Page.vue
@@ -2,23 +2,15 @@
   <v-container fluid>
     <v-row>
       <v-col cols>
-        <MyCard
-          title="Login"
-          subTitle="Welcome back. Please login to our service"
-          width="500"
-          elevation="2"
-        />
+        <slot />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import MyCard from "./Card";
-
 export default {
   name: "my-page",
-  components: { MyCard },
   methods: {
     onLogin() {
       this.$emit("onLogin");
